7b2c88517f320cd026118176261337f783f6302b
stacydbkim <127383386+stacydbkim@users.noreply.github.com>
2023-05-14 14:32:04 +0900

    java

--- whatsinmybag/js/java.js
+++ whatsinmybag/js/java.js
@@ -34,4 +34,24 @@ images.forEach(image => {
         dragItem.style.top = (event.clientY - offsetY) + "px";
       }
     }
+
+
     
+var autoplayVideoInterval = setInterval("autoplayVideo()",200);
+
+
+function autoplayVideo()
+{
+var promise = document.querySelector('video').play();
+if (promise !== undefined)
+{
+promise.then(function (_)
+{
+// Autoplay started!
+clearInterval(autoplayVideoInterval);
+
+}).catch(function (error) {// Autoplay was prevented.
+// Show a "Play" button so that user can start playback.
+});
+}
+}
