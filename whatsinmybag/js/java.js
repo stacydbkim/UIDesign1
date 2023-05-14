78a0eb8a0f7aa9a2caaf0b859634496643d5f366
stacydbkim <127383386+stacydbkim@users.noreply.github.com>
2023-05-14 14:33:11 +0900

    java delete

--- whatsinmybag/js/java.js
+++ whatsinmybag/js/java.js
@@ -35,23 +35,3 @@ images.forEach(image => {
       }
     }
 
-
-    
-var autoplayVideoInterval = setInterval("autoplayVideo()",200);
-
-
-function autoplayVideo()
-{
-var promise = document.querySelector('video').play();
-if (promise !== undefined)
-{
-promise.then(function (_)
-{
-// Autoplay started!
-clearInterval(autoplayVideoInterval);
-
-}).catch(function (error) {// Autoplay was prevented.
-// Show a "Play" button so that user can start playback.
-});
-}
-}
